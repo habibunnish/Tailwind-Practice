f2ee1843dbccf73dd5f23a0a52cf96b6a8914bee
nisha.afroz <nisha.afroz@aspiresys.com>
2025-09-02 16:21:01 +0530

    clipboard website project 1 using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  // adjust paths if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
